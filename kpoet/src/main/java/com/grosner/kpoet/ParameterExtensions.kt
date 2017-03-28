2d5a66390fbd546033686adaf6e50922364d9c32
fuzzagrosner <andrew@fuzzproductions.com>
2017-03-28 16:48:16 -0400

    [annotations] add method that will tap a map of assignments and convert those to annotations applied to the annotatiioon to attempt to map to java closely.

--- kpoet/src/main/java/com/grosner/kpoet/ParameterExtensions.kt
+++ kpoet/src/main/java/com/grosner/kpoet/ParameterExtensions.kt
@@ -10,8 +10,8 @@ import kotlin.reflect.KClass
 fun param(annotationFunction: ParamMethod, kClass: KClass<*>, name: String)
         = ParameterSpec.builder(kClass.java, name)!!.annotationFunction()
 
-fun param(annotationSpec: AnnotationSpec, kClass: KClass<*>, name: String)
-        = ParameterSpec.builder(kClass.java, name).addAnnotation(annotationSpec)!!
+fun param(annotationSpec: AnnotationSpec.Builder, kClass: KClass<*>, name: String)
+        = ParameterSpec.builder(kClass.java, name).addAnnotation(annotationSpec.build())!!
 
 fun param(kClass: KClass<*>, name: String) = ParameterSpec.builder(kClass.java, name)!!
 
@@ -20,12 +20,15 @@ fun param(typeName: TypeName, name: String) = ParameterSpec.builder(typeName, na
 fun `final param`(annotationFunction: ParamMethod, kClass: KClass<*>, name: String)
         = ParameterSpec.builder(kClass.java, name).addModifiers(Modifier.FINAL)!!.annotationFunction()
 
-fun `final param`(annotationSpec: AnnotationSpec, kClass: KClass<*>, name: String)
-        = ParameterSpec.builder(kClass.java, name).addModifiers(Modifier.FINAL)!!.addAnnotation(annotationSpec)
+fun `final param`(annotationSpec: AnnotationSpec.Builder, kClass: KClass<*>, name: String)
+        = ParameterSpec.builder(kClass.java, name).addModifiers(Modifier.FINAL)
+        .addAnnotation(annotationSpec.build())!!
 
-fun `final param`(kClass: KClass<*>, name: String) = ParameterSpec.builder(kClass.java, name).addModifiers(Modifier.FINAL)!!
+fun `final param`(kClass: KClass<*>, name: String)
+        = ParameterSpec.builder(kClass.java, name).addModifiers(Modifier.FINAL)!!
 
-fun `final param`(typeName: TypeName, name: String) = ParameterSpec.builder(typeName, name).addModifiers(Modifier.FINAL)!!
+fun `final param`(typeName: TypeName, name: String)
+        = ParameterSpec.builder(typeName, name).addModifiers(Modifier.FINAL)!!
 
 fun ParameterSpec.Builder.`@`(kClass: KClass<*>, annotationMethod: AnnotationMethod = { this })
         = addAnnotation(AnnotationSpec.builder(kClass.java).annotationMethod().build())!!
@@ -33,11 +36,20 @@ fun ParameterSpec.Builder.`@`(kClass: KClass<*>, annotationMethod: AnnotationMet
 fun ParameterSpec.Builder.`@`(className: ClassName, annotationMethod: AnnotationMethod = { this })
         = addAnnotation(AnnotationSpec.builder(className).annotationMethod().build())!!
 
-fun `@`(kClass: KClass<*>, annotationMethod: AnnotationMethod = { this })
-        = AnnotationSpec.builder(kClass.java).annotationMethod().build()!!
+fun `@`(kClass: KClass<*>, mapFunc: MutableMap<String, String>.() -> Unit = { })
+        = AnnotationSpec.builder(kClass.java)
+        .apply {
+            mutableMapOf<String, String>().apply { mapFunc(this) }
+                    .forEach { key, value -> addMember(key, value) }
+        }
 
-fun `@`(className: ClassName, annotationMethod: AnnotationMethod = { this })
-        = AnnotationSpec.builder(className).annotationMethod().build()!!
+
+fun `@`(className: ClassName, mapFunc: MutableMap<String, String>.() -> Unit = {})
+        = AnnotationSpec.builder(className)
+        .apply {
+            mutableMapOf<String, String>().apply { mapFunc(this) }
+                    .forEach { key, value -> addMember(key, value) }
+        }
 
 
 
--- kpoet/src/test/java/com/grosner/kpoet/ParameterExtensionsTest.kt
+++ kpoet/src/test/java/com/grosner/kpoet/ParameterExtensionsTest.kt
@@ -9,19 +9,16 @@ import org.jetbrains.spek.api.dsl.on
 import org.junit.Assert.assertEquals
 
 class ParameterExtensionsTest : Spek({
-
-
     describe("tests parameter extension methods and sample their features") {
-
         on("generate java file") {
             val file = javaFile("com.grosner") {
                 `class`("Destroyer") {
                     `public`(TypeName.VOID, "welcomeOverlords",
-                            param(`@`(Nullable::class, { member("value", "".S) }), String::class, "robot"),
-                            `final param`({ `@`(Nullable::class) }, String::class, "android"),
+                            param(`@`(Nullable::class, { this["value"] = "".S }), String::class, "robot"),
+                            `final param`(`@`(Nullable::class), String::class, "android"),
                             param(`@`(TestAnnotation::class, {
-                                member("name", "Some Kind of Member".S)
-                                member("purpose", "Some Purpose we have".S)
+                                this["name"] = "Some Kind of Member".S
+                                this["purpose"] = "Some Purpose we have".S
                             }), Int::class, "purposeFul"))
                 }
             }
